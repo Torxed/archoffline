6d2f7f04de59904e5ca11d6d624ca9cab514113b
Anton Hvornum <anton@hvornum.se>
2022-11-24 15:06:14 +0100

    Fixed mypy complaints

--- archoffline/__main__.py
+++ archoffline/__main__.py
@@ -1,15 +1,16 @@
-import importlib
+import importlib.util
 import sys
 import pathlib
 
 # Load .git version before the builtin version
 if pathlib.Path('./archoffline/__init__.py').absolute().exists():
-	spec = importlib.util.spec_from_file_location("archoffline", "./archoffline/__init__.py")
-	archoffline = importlib.util.module_from_spec(spec)
-	sys.modules["archoffline"] = archoffline
-	spec.loader.exec_module(sys.modules["archoffline"])
+	if spec := importlib.util.spec_from_file_location("archoffline", "./archoffline/__init__.py"):
+		archoffline = importlib.util.module_from_spec(spec)
+		sys.modules["archoffline"] = archoffline
+		if spec.loader:
+			spec.loader.exec_module(sys.modules["archoffline"])
 else:
-	import archoffline
+	import archoffline # type: ignore[no-redef]
 
 if __name__ == '__main__':
 	archoffline.main()
